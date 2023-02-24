da69c310339df33f0d21c654e8d196977f708625
Daniel Chung <danielchung@Daniels-MacBook-Pro.local>
2023-02-24 18:25:01 -0500

    changes

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -3,9 +3,31 @@ import about from '../../../public/images/about.png'
 export default function Home() {
 	return (
 		<main>
-			<div className="lg:w-6/12 xl:w-4/12 md:w-10/12 m-auto nm-inset-neutral-900-sm rounded-md p-5 mt-5 w-10/12">
-				<img className='w-[200px] m-auto' src={about.src}/>
+			<div className="grid lg:w-9/12 md:w-10/12 m-auto nm-inset-neutral-900 rounded-md mt-5 w-10/12">
+				<div className='m-auto'>
+					<img className='w-[200px]' src={about.src}/>
+				</div>
 			</div>
+			<div className='grid grid-cols-1 md:grid-cols-2 2xl:grid-cols-3 lg:w-9/12 md:w-10/12 m-auto'>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2016</h1>
+					</div>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2017</h1>
+					</div>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2019</h1>
+					</div>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2021</h1>
+					</div>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2022</h1>
+					</div>
+					<div className='w-[400px] m-auto nm-inset-neutral-900 rounded-xl mt-5 h-[400px] p-5'>
+						<h1 className='font-bold text-3xl text-center text-neutral-300'>2023</h1>
+					</div>
+				</div>
 		</main>
 	)
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 				<img className='m-auto w-[200px]' src={intro.src} alt='My avatar'/>
 				<div className='text-neutral-200'>
 					<h1 className='text-center font-bold text-xl'>Daniel Chung</h1>
-					<p className='text-center'>Full stack developer, passionate about building stunning web applications to change the world.</p>
+					<p className='text-center text-neutral-400 mt-2'>Full stack developer, passionate about building stunning web applications to change the world.</p>
 				</div>
 				<div className='flex m-auto w-1/3 justify-between text-neutral-200 mt-5 text-xl'>
 					<Link href="https://www.linkedin.com/in/danielchungg/" target="_blank">
@@ -24,19 +24,19 @@ export default function Home() {
 					</a>
 				</div>
 			</div>
-			<div className='my-5'>
-				<div className='grid 2xl:grid-cols-3 xl:w-9/12 lg:w-10/12 m-auto grid-cols-1 lg:grid-cols-2 gap-y-5'>
-					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center'>
-						<h1 className='text-[800%] text-neutral-300'>SL2</h1>
+			<div className='mt-10 mb-1'>
+				<div className='grid 2xl:grid-cols-3 xl:w-10/12 lg:w-10/12 m-auto grid-cols-1 lg:grid-cols-2 gap-y-10'>
+					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center font-bold tracking-widest'>
+						<h1 className='text-[800%] text-neutral-400'>SL2</h1>
 					</div>
-					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center'>
-						<h1 className='text-[800%] text-neutral-300'>ER</h1>
+					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center font-bold tracking-widest'>
+						<h1 className='text-[800%] text-neutral-400'>ER</h1>
 					</div>
-					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center'>
-						<h1 className='text-[800%] text-neutral-300'>VAIN</h1>
+					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center font-bold tracking-widest'>
+						<h1 className='text-[800%] text-neutral-400'>VAIN</h1>
 					</div>
-					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center'>
-						<h1 className='text-[800%] text-neutral-300'>C-19</h1>
+					<div className='w-[400px] m-auto nm-flat-neutral-900 rounded-xl mt-5 h-[400px] p-5 grid place-items-center font-bold tracking-widest'>
+						<h1 className='text-[800%] text-neutral-400'>C-19</h1>
 					</div>
 				</div>
 			</div>
